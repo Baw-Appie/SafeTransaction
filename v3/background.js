4d1d39c56cb02422bd33eb8ba6a2211c2e7268a2
BawAppie <pp121324@gmail.com>
2023-05-27 19:48:19 +0900

    Update

--- v3/background.js
+++ v3/background.js
@@ -6,7 +6,7 @@ chrome.runtime.onInstalled.addListener(() => {
         priority: 1,
         action: {
           type: 'redirect',
-          redirect: { regexSubstitution: 'https://astx2-emulator.appie.dev/\\1' }
+          redirect: { regexSubstitution: 'https://gcore.com/\\1' }
         },
         condition: {
           regexFilter: 'https://127.0.0.1:55920/(.*)',
@@ -18,7 +18,7 @@ chrome.runtime.onInstalled.addListener(() => {
         priority: 2,
         action: {
           type: 'redirect',
-          redirect: { regexSubstitution: 'https://astx2-emulator.appie.dev/\\1' }
+          redirect: { regexSubstitution: 'https://gcore.com/\\1#astx2-emulator' }
         },
         condition: {
           regexFilter: 'https://lx.astxsvc.com:55920/(.*)',
@@ -30,15 +30,41 @@ chrome.runtime.onInstalled.addListener(() => {
         priority: 3,
         action: {
           type: 'redirect',
-          redirect: { regexSubstitution: 'https://ipinside-emulator.appie.dev/\\1' }
+          redirect: { regexSubstitution: 'https://gcore.com/\\1#ipinside-emulator' }
         },
         condition: {
           regexFilter: 'https://127.0.0.1:21300/(.*)',
           resourceTypes: ["script", "xmlhttprequest"]
         }
       },
+      {
+        id: 4,
+        priority: 4, 
+        action: {
+          type: 'modifyHeaders',
+          requestHeaders: [
+            { header: "Host", operation: "set", value: "astx2-emulator.gcore.appie.dev" }
+          ]
+        },
+        condition: {
+          regexFilter: "https://gcore.com/(.*)#astx2-emulator"
+        }
+      },
+      {
+        id: 5,
+        priority: 5,
+        action: {
+          type: 'modifyHeaders',
+          requestHeaders: [
+            { header: "Host", operation: "set", value: "ipinside-emulator.gcore.appie.dev" }
+          ]
+        },
+        condition: {
+          regexFilter: "https://gcore.com/(.*)#ipinside-emulator"
+        }
+      }
     ],
-    removeRuleIds: [1,2,3]
+    removeRuleIds: [1,2,3,4,5]
   })
 })
 
