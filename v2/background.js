10188ef140edbd6febf64fa064790044be800ba9
BawAppie <pp121324@gmail.com>
2023-05-27 18:06:20 +0900

    Update

--- v2/background.js
+++ v2/background.js
@@ -6,11 +6,11 @@ browser.webRequest.onBeforeRequest.addListener(
     const url = new URL(details.url);
     const { requestId } = details
     if(url.host === "127.0.0.1:55920" || url.host === "lx.astxsvc.com:55920") {
-      astx2Requests.add(requestId)
+      astx2Requests.add(url.pathname + url.search)
       return { redirectUrl: "https://gcore.com" + url.pathname + url.search };
     }
     if(url.host === "127.0.0.1:21300") {
-      ipinsideRequests.add(requestId)
+      ipinsideRequests.add(url.pathname + url.search)
       return { redirectUrl: "https://gcore.com" + url.pathname + url.search };
     }
   },
@@ -21,11 +21,13 @@ browser.webRequest.onBeforeRequest.addListener(
 
 browser.webRequest.onBeforeSendHeaders.addListener(
   details => { 
+    const url = new URL(details.url);
     let { requestHeaders, requestId } = details
-    if(!astx2Requests.has(requestId) && !ipinsideRequests.has(requestId)) return
+    if(url.host !== "gcore.com") return
+    if(!astx2Requests.has(url.pathname + url.search) && !ipinsideRequests.has(url.pathname + url.search)) return
     requestHeaders = requestHeaders.map((header) => {
       if (header.name === 'Host') {
-        return { name: header.name, value: astx2Requests.has(requestId) ? "astx2-emulator.gcore.appie.dev" : "ipinside-emulator.gcore.appie.dev" };
+        return { name: header.name, value: astx2Requests.has(url.pathname + url.search) ? "astx2-emulator.gcore.appie.dev" : "ipinside-emulator.gcore.appie.dev" };
       }
       return header;
     });
