92c4ec47d9afe2bd24af0d18fee986e1206d8e46
BawAppie <pp121324@gmail.com>
2023-05-27 17:22:32 +0900

    Update

--- v2/background.js
+++ v2/background.js
@@ -1,20 +1,39 @@
+const astx2Requests = new Set()
+const ipinsideRequests = new Set()
+
 browser.webRequest.onBeforeRequest.addListener(
   details => {
     const url = new URL(details.url);
-    if(url.host === "127.0.0.1:55920" || url.host === "lx.astxsvc.com:55920") return { redirectUrl: "https://astx2-emulator.appie.dev" + url.pathname + url.search };
+    const { requestId } = details
+    if(url.host === "127.0.0.1:55920" || url.host === "lx.astxsvc.com:55920") {
+      astx2Requests.add(requestId)
+      return { redirectUrl: "https://gcore.com" + url.pathname + url.search };
+    }
+    if(url.host === "127.0.0.1:21300") {
+      ipinsideRequests.add(requestId)
+      return { redirectUrl: "https://gcore.com" + url.pathname + url.search };
+    }
   },
-  {
-    urls: ["<all_urls>"]
-  },
+  { urls: ["<all_urls>"] },
   ["blocking"]
 )
-browser.webRequest.onBeforeRequest.addListener(
-  details => {
-    const url = new URL(details.url);
-    if(url.host === "127.0.0.1:21300") return { redirectUrl: "https://ipinside-emulator.appie.dev" + url.pathname + url.search };
+
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  details => { 
+    let { requestHeaders, requestId } = details
+    if(!astx2Requests.has(requestId) && !ipinsideRequests.has(requestId)) return
+    requestHeaders = requestHeaders.map((header) => {
+      if (header.name === 'Host') {
+        return { name: header.name, value: astx2Requests.has(requestId) ? "astx2-emulator.gcore.appie.dev" : "ipinside-emulator.gcore.appie.dev" };
+      }
+      return header;
+    });
+    
+    return { rquestHeaders }
   },
   {
     urls: ["<all_urls>"]
   },
-  ["blocking"]
+  ["blocking", "requestHeaders"]
 )
