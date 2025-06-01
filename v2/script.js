fdb351a8844af88e7fd63fddbbff8de479ab8fcc
Baw-Appie <pp121324@gmail.com>
2025-06-01 16:48:02 +0900

    Add Hometax Patch

--- v2/script.js
+++ v2/script.js
@@ -76,23 +76,40 @@ const _SafeTransactionKBankBizPatcher = () => {
   const originalSend = XMLHttpRequest.prototype.send;
 
   XMLHttpRequest.prototype.open = function (method, url) {
-    this._target = (typeof url === 'string' && url.startsWith("/product/initech/crossweb/webui/conf/customerConf.json"));
     this._url = url;
     return originalOpen.apply(this, arguments);
   };
 
   XMLHttpRequest.prototype.send = function () {
-    if (this._target) {
-      this.addEventListener('readystatechange', function (e) {
-        if (this.readyState === 4) {
-          const newText = this.responseText.replaceAll(`"USE":"Y"`, `"USE":"N"`)
-          Object.defineProperty(this, 'responseText', {
-            get: function () { return newText; }
-          });
-          Object.defineProperty(this, 'response', {
-            get: function () { return newText; }
-          });
-        }
+    if (typeof this._url === 'string' && this._url.startsWith("/product/initech/crossweb/webui/conf/customerConf.json")) {
+      this.addEventListener('readystatechange', e => {
+        if (this.readyState != 4) return
+        const newText = this.responseText.replaceAll(`"USE":"Y"`, `"USE":"N"`)
+        Object.defineProperty(this, 'responseText', { get: () => { return newText; } });
+        Object.defineProperty(this, 'response', { get: () => { return newText; } });
+      });
+    }
+    return originalSend.apply(this, arguments);
+  };
+}
+
+const _SafeTransactionHomeTaxPatcher = () => {
+  if (location.host != "hometax.go.kr") return
+  const originalOpen = XMLHttpRequest.prototype.open;
+  const originalSend = XMLHttpRequest.prototype.send;
+
+  XMLHttpRequest.prototype.open = function (method, url) {
+    this._url = url;
+    return originalOpen.apply(this, arguments);
+  };
+
+  XMLHttpRequest.prototype.send = function () {
+    if (typeof this._url === 'string' && this._url.includes("js/comm/raonsecure/transkey/rsa_oaep_files/rsa_oaep-min.js")) {
+      console.log("[SafeTransaction] Patching XMLHttpRequest for HomeTax RSA OAEP");
+      this.addEventListener('readystatechange', e => {
+        if (this.readyState != 4) return
+        Object.defineProperty(this, 'statusText', { get: () => { return "Internal Server Error"; } });
+        Object.defineProperty(this, 'status', { get: () => { return 500; } });
       });
     }
     return originalSend.apply(this, arguments);
@@ -101,6 +118,7 @@ const _SafeTransactionKBankBizPatcher = () => {
 
 // _SafeTransactionNOSVKeypadPatchInitializer()
 _SafeTransactionKBankBizPatcher()
+_SafeTransactionHomeTaxPatcher()
 const _SafeTransactionOnWindowLoaded = () => {
   _SafeTransactionUserAgentPatcher()
 }
